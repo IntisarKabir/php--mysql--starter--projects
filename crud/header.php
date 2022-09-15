4c109481b86bbf4e0e32fa1dd02bcc94e84d2856
IntisarKabir <75949278+IntisarKabir@users.noreply.github.com>
2022-09-16 02:05:41 +0600

    Add files via upload

---
+++ crud/header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP CRUD using jquery ajax without page reload</title>
+    <?php require_once "cdn/set1.html" ;   ?>
+
+   
+</head>
+<body>
